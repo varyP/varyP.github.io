d2e170f6cfce254210799e97775702a9d5f5596b
Varun <varun.parakh07@gmail.com>
2020-08-08 12:53:13 +0800

    Updated base url

--- Sources/varyPGithubIo/main.swift
+++ Sources/varyPGithubIo/main.swift
@@ -16,7 +16,7 @@ struct varyPGithubIo: Website {
     }
 
     // Update these properties to configure your website:
-    var url = URL(string: "https://varyP.github.io")!
+    var url = URL(string: "https://varyp.github.io/Blog")!
     var name = "Varun's Blog"
     var description = "I'm Varun Parakh. A Software Development Engineer with expertise in Mobile Applications, Payments & Gamification. With apps featured on Apple's App Store & Google Play Store, i've worked on mobile apps with millions of downloads and mass reach to audiences from States & EU to India. My key skills sets includes designing and development of mobile applications & real-time multi-player mobile games that can serve millions of users on a daily basis. Some of the technologies/languages that I use in my daily life are Swift, Rx, Objective-C, C++, Cocos-2DX, Restful web services, GCP, SQL-Lite, Performance Monitoring tools, Project Management Tool - JIRA, Git, Slack. "
     var language: Language { .english }
